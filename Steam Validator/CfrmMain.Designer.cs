762886e9790c4562b8d71a420d344fe16c89b2e3
Ian Nieuwoudt <inieuwoudt1pc@live.com>
2022-04-11 18:59:22 +0200

    Add project files.

---
+++ Steam Validator/CfrmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Steam_Validator
+{
+    partial class CfrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CfrmMain));
+            this.lblLibFoldersPath = new System.Windows.Forms.Label();
+            this.txtLibFoldersPath = new System.Windows.Forms.TextBox();
+            this.btnSelectLibFoldersPath = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nuMaxTries = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nuMaxTries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLibFoldersPath
+            // 
+            this.lblLibFoldersPath.AutoSize = true;
+            this.lblLibFoldersPath.Location = new System.Drawing.Point(12, 15);
+            this.lblLibFoldersPath.Name = "lblLibFoldersPath";
+            this.lblLibFoldersPath.Size = new System.Drawing.Size(86, 13);
+            this.lblLibFoldersPath.TabIndex = 0;
+            this.lblLibFoldersPath.Text = "libraryfolders.vdf:";
+            // 
+            // txtLibFoldersPath
+            // 
+            this.txtLibFoldersPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLibFoldersPath.Location = new System.Drawing.Point(104, 12);
+            this.txtLibFoldersPath.Name = "txtLibFoldersPath";
+            this.txtLibFoldersPath.ReadOnly = true;
+            this.txtLibFoldersPath.Size = new System.Drawing.Size(313, 20);
+            this.txtLibFoldersPath.TabIndex = 0;
+            this.txtLibFoldersPath.TabStop = false;
+            this.txtLibFoldersPath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSelectLibFoldersPath
+            // 
+            this.btnSelectLibFoldersPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectLibFoldersPath.Location = new System.Drawing.Point(419, 11);
+            this.btnSelectLibFoldersPath.Name = "btnSelectLibFoldersPath";
+            this.btnSelectLibFoldersPath.Size = new System.Drawing.Size(24, 22);
+            this.btnSelectLibFoldersPath.TabIndex = 0;
+            this.btnSelectLibFoldersPath.Text = "...";
+            this.btnSelectLibFoldersPath.UseVisualStyleBackColor = true;
+            this.btnSelectLibFoldersPath.Click += new System.EventHandler(this.BtnSelectLibFoldersPath_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnVerify.Location = new System.Drawing.Point(371, 47);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(72, 27);
+            this.btnVerify.TabIndex = 1;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.BtnVerify_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.Filter = "libraryfolders.vdf|libraryfolders.vdf";
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Note: Run Steam as Administrator to prevent confirmation dialogs from interruptin" +
+    "g the process.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nuMaxTries
+            // 
+            this.nuMaxTries.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nuMaxTries.Location = new System.Drawing.Point(296, 53);
+            this.nuMaxTries.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuMaxTries.Name = "nuMaxTries";
+            this.nuMaxTries.Size = new System.Drawing.Size(69, 20);
+            this.nuMaxTries.TabIndex = 3;
+            this.nuMaxTries.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nuMaxTries.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Max Tries:";
+            // 
+            // CfrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 81);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nuMaxTries);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnSelectLibFoldersPath);
+            this.Controls.Add(this.txtLibFoldersPath);
+            this.Controls.Add(this.lblLibFoldersPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CfrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steam Validator";
+            ((System.ComponentModel.ISupportInitialize)(this.nuMaxTries)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLibFoldersPath;
+        private System.Windows.Forms.TextBox txtLibFoldersPath;
+        private System.Windows.Forms.Button btnSelectLibFoldersPath;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nuMaxTries;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Steam Validator/CfrmMain.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steam_Validator
+{
+    public partial class CfrmMain : Form
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        int MaxTries = 3;
+
+        public CfrmMain()
+        {
+            InitializeComponent();
+
+            Prepare();
+        }
+
+        void ToggleUI(bool enabled)
+        {
+            Invoke((Action)delegate
+            {
+                foreach (Control control in Controls)
+                    control.Enabled = enabled;
+            });
+        }
+
+        void UpdateText(string text = "")
+        {
+            Invoke((Action)delegate
+            {
+                if (!string.IsNullOrEmpty(text))
+                    Text = "Steam Validator - " + text;
+                else
+                    Text = "Steam Validator";
+            });
+        }
+
+        void Prepare()
+        {
+            string Letters = "CABDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < Letters.Length; i++)
+            {
+                string Path = Letters[i] + @":\Program Files (x86)\Steam\steamapps\libraryfolders.vdf";
+                
+                if (File.Exists(Path))
+                {
+                    txtLibFoldersPath.Text = Path;
+                    break;
+                }
+
+                Path = Letters[i] + @":\SteamLibrary\steamapps\libraryfolders.vdf";
+
+                if (File.Exists(Path))
+                {
+                    txtLibFoldersPath.Text = Path;
+                    break;
+                }
+
+                Path = Letters[i] + @":\Steam\steamapps\libraryfolders.vdf";
+
+                if (File.Exists(Path))
+                {
+                    txtLibFoldersPath.Text = Path;
+                    break;
+                }
+            }
+        }
+
+        void Verify()
+        {
+            string Path = txtLibFoldersPath.Text;
+            MaxTries = (int)nuMaxTries.Value;
+
+            List<string> Failures = new List<string>();
+
+            Thread thread = new Thread(async () =>
+            {
+                ToggleUI(false);
+
+                string[] libraryfolders = File.ReadAllLines(Path);
+                List<string> AppIDs = new List<string>();
+
+                for (int i = 0; i < libraryfolders.Length; i++)
+                {
+                    string line = libraryfolders[i];
+                    if (line.StartsWith("\t\t\t"))
+                        AppIDs.Add(line.Split(new string[] { "\t", "" }, StringSplitOptions.RemoveEmptyEntries)[0].Replace("\"", ""));
+                }
+
+                AppIDs.Sort();
+
+                for (int i = 0; i < AppIDs.Count; i++)
+                {
+                    bool success = false;
+                    int _try = 0;
+
+                    while (!success && _try++ < MaxTries)
+                    {
+                        try
+                        {
+                            UpdateText("Verifying " + (i + 1) + "/" + AppIDs.Count + " (0%)...");
+
+                            ProcessStartInfo psi = new ProcessStartInfo("steam://validate/" + AppIDs[i]);
+
+                            Process verify = new Process();
+                            verify.StartInfo = psi;
+                            verify.Start();
+
+                            Process verifyWindow = new Process();
+                            bool assigned = false;
+
+                            while (!assigned)
+                            {
+                                Process[] steams = Process.GetProcessesByName("steam");
+
+                                for (int j = 0; j < steams.Length; j++)
+                                {
+                                    if (steams[j].MainWindowTitle.ToLower().StartsWith("validating steam files"))
+                                    {
+                                        ShowWindow(steams[j].MainWindowHandle, ShowWindowEnum.ForceMinimized);
+                                        verifyWindow = steams[j];
+                                        assigned = true;
+                                        break;
+                                    }
+                                }
+
+                                await Task.Delay(50);
+                            }
+
+
+                            int Tries = 0;
+
+                            string PreviousMainWindowTitle = "";
+
+                            while (true)
+                            {
+                                string MainWindowTitle = verifyWindow.MainWindowTitle.ToLower();
+
+                                string Percentage = "";
+
+                                try
+                                {
+                                    Percentage = MainWindowTitle.Substring("validating steam files - ".Length);
+                                    Percentage = Percentage.Substring(0, Percentage.IndexOf('%'));
+                                    UpdateText("Verifying " + (i + 1) + "/" + AppIDs.Count + " ("+ Percentage + "%)...");
+                                }
+                                catch { }
+
+                                if (MainWindowTitle == PreviousMainWindowTitle)
+                                {
+                                    if (++Tries == 120)
+                                    {
+                                        verifyWindow.CloseMainWindow();
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    PreviousMainWindowTitle = MainWindowTitle;
+                                    Tries = 0;
+                                }
+
+                                if (MainWindowTitle.EndsWith(" 100% complete"))
+                                {
+                                    verifyWindow.CloseMainWindow();
+                                    success = true;
+                                    break;
+                                }
+
+                                await Task.Delay(500);
+                                verifyWindow.Refresh();
+                            }
+
+                            await Task.Delay(5000);
+                        }
+                        catch { }
+                    }
+
+                    if (!success)
+                        Failures.Add(AppIDs[i]);
+                }
+
+                UpdateText();
+                ToggleUI(true);
+
+                Invoke((Action)delegate
+                {
+                    WindowState = FormWindowState.Normal;
+                    TopMost = true;
+                    TopMost = false;
+
+                    if (Failures.Count > 0)
+                        MessageBox.Show("Validation Complete\n\nFailed AppIDs:\n" + string.Join("\n", Failures), "Steam Validator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else
+                        MessageBox.Show("Validation Complete", "Steam Validator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                });
+            });
+            thread.Start();
+        }
+
+        private void BtnSelectLibFoldersPath_Click(object sender, EventArgs e)
+        {
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+                txtLibFoldersPath.Text = dlgOpen.FileName;
+
+            dlgOpen.FileName = string.Empty;
+        }
+
+        private void BtnVerify_Click(object sender, EventArgs e)
+        {
+            Verify();
+        }
+    }
+
+    public enum ShowWindowEnum
+    {
+        Hide = 0,
+        ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+        Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+        Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+        Restore = 9, ShowDefault = 10, ForceMinimized = 11
+    };
+
+}
